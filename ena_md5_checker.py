ddbc891c0c13d6125944caa34323f829761dc123
Hanzhou Wang <whzlp0430@yahoo.com>
2023-10-10 13:38:43 -0500

    adding tqdm to md5_checker

--- ena_md5_checker.py
+++ ena_md5_checker.py
@@ -1,27 +1,25 @@
-import pandas #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import argparse
 import pandas as pd
 import re
 import subprocess
 from pathlib import Path
+from tqdm import tqdm
 
 def tsv_to_dict(filename: str) -> dict:
-    """Convert columns from a TSV file into a dictionary."""
     df = pd.read_csv(filename, sep='\t')
     if 'experiment_alias' not in df.columns or 'fastq_md5' not in df.columns:
         raise ValueError("The TSV file must contain both 'experiment_alias' and 'fastq_md5' columns.")
     return df.set_index('experiment_alias')['fastq_md5'].to_dict()
 
 def extract_sample_id(filename: str) -> str:
-    """Extract the GSM identifier from a given filename."""
     match = re.search(r'GSM\d+', filename)
     if not match:
         raise ValueError(f"No GSM identifier found in filename: {filename}")
     return match.group(0)
 
 def _verify_one_md5(file_path: Path, md5_dict: dict) -> bool:
-    """Verify the MD5 hash of a file against a provided dictionary using the Linux md5sum utility."""
     result = subprocess.run(['md5sum', file_path], capture_output=True, text=True)
     calculated_md5 = result.stdout.split()[0]
     sample_id = extract_sample_id(file_path.name)
@@ -31,14 +29,16 @@ def _verify_one_md5(file_path: Path, md5_dict: dict) -> bool:
     return calculated_md5 == expected_md5
 
 def main(args):
-    """Main function to verify MD5 hashes for FASTQ files using a provided TSV file."""
     md5_dict = tsv_to_dict(args.tsv_file)
     unmatched_sample_ids = []
-    for file in Path(args.fastq_folder).glob("*.fastq.gz"):
+
+    # Use tqdm for progress bar
+    for file in tqdm(list(Path(args.fastq_folder).glob("*.fastq.gz")), desc="Verifying MD5"):
         if not _verify_one_md5(file, md5_dict):
             sample_id = extract_sample_id(file.name)
-            print(f"Unmatched MD5 for sample: {sample_id}")
+            print(f"\nUnmatched MD5 for sample: {sample_id}")
             unmatched_sample_ids.append(sample_id)
+
     return unmatched_sample_ids
 
 if __name__ == "__main__":
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     unmatched = main(args)
     if unmatched:
-        print("\nSample IDs with unmatched MD5 values:")
+        print("\nSamples with unmatched MD5 values:")
         for sample_id in unmatched:
             print(sample_id)
     else:
